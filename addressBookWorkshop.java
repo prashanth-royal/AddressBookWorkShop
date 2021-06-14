9dc301af6544c0c47fc2cf7a0fbed5350de11d7f
Prashanth <prashanth.royal2046@gmail.com>
2021-06-14 08:33:03 +0530

    Refactor: welcome message

--- addressBookWorkshop.java
+++ addressBookWorkshop.java
@@ -1,1 +1,6 @@
-Welcome to Address Book Solution Problem
+public class AddressBookMain {
+
+	public static void main(String[] args) {
+        System.out.println("Welcome to Address Book Problem");
+	}
+}
